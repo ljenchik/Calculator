b39a3597a44d411b2053c46f8dc1e618f6d8e259
aaxddppp <aaxddppp@email.com>
2022-04-11 23:31:02 +0100

    Added validation with out parameter

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,48 +4,56 @@
     {
         static void Main(string[] args)
         {
+            PrintWelcomeMessage();
+            PerformOneCalcultion();
+            Console.ReadLine();
+        }
+        
+        private static void PrintWelcomeMessage()
+        {
             Console.WriteLine("Welcome to the calculator!");
             Console.WriteLine("=========================");
-            
+        }
+        
+        private static int EnterInformation(string message)
+        {
+            Console.WriteLine(message);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+                Console.WriteLine("Enter an integer");
+            }
+        }
+        
+        private static void PerformOneCalcultion()
+        {
             Console.WriteLine("Please enter the operator +, -, / or *: ");
             char math_sign = char.Parse(Console.ReadLine());
             
-            Console.WriteLine("How many numbers do you want to " + $"{math_sign}" + " ?: ");
-            int quantity = int.Parse(Console.ReadLine());
-
-            decimal[] numbers = new decimal[quantity];
+            int quantity = EnterInformation("How many numbers do you want to " + $"{math_sign}" + " ?: ");
+            int[] numbers = new int[quantity];
             
             for (int i = 0; i < quantity; i++)
-            {
-                Console.Write("Please enter number: ");
-                string num_str = Console.ReadLine();
-                decimal number = decimal.Parse(num_str);
-                numbers[i] = number;
-            }
-
+                numbers[i] = EnterInformation("Please enter number: ");
             decimal res = numbers[0];
             for (int i = 1; i < quantity; i++)
             {
                 if (math_sign == '+')
-                {
                     res += numbers[i];
-                }
                 else if (math_sign == '-')
-                {
                     res -= numbers[i];
-                }
                 else if (math_sign == '/')
-                {
                     res /= numbers[i];
-                }
                 else if (math_sign == '*')
-                {
                     res *= numbers[i];
-                }
             }
-            Console.WriteLine("The answer is {0}",res);
-            Console.ReadLine();
+            Console.WriteLine("The answer is {0}", res);
+            PerformOneCalcultion();
         }
     }
 }
+
 
