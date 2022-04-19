4c36f5bc579e0a36b99e2d93791c38df77b5dcde
aaxddppp <aaxddppp@email.com>
2022-04-19 22:34:50 +0100

    Added enum and switch

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,102 +1,92 @@
-﻿using System.Globalization;
+﻿namespace Calculator;
 
-namespace Calculator
+internal class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        private const int NumberOneCalculator = 1;
-        private const int DateOneCalculator = 2;
-
-        static void Main(string[] args)
+        PrintWelcomeMessage();
+        while (true)
         {
-            PrintWelcomeMessage();
-            while (true)
+            var calculationMode = AskForCalculationMode();
+            if (calculationMode == (int) CalculatorMode.Numbers)
             {
-                int calculationMode = AskForCalculationMode();
-                if (calculationMode == NumberOneCalculator)
-                {
-                    NumberCalculator.PerformNumberCalcultion();
-                }
-                else if (calculationMode == DateOneCalculator)
-                {
-                    DateCalculator.PerformeDateCalculation();
-                }
+                NumberCalculator.PerformNumberCalcultion();
             }
-        }
-
-        private static int AskForCalculationMode()
-        {
-            Console.WriteLine("Which calculator do you want?");
-            Console.WriteLine("For Numbers enter 1");
-            Console.WriteLine("For Dates enter 2");
-            int choice = int.Parse(Console.ReadLine());
-            return choice;
-        }
-
-        private static void PrintWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the calculator!");
-            Console.WriteLine("=========================");
-        }
-
-        class NumberCalculator
-        {
-            public static void PerformNumberCalcultion()
+            else if (calculationMode == (int) CalculatorMode.Dates)
             {
-                Console.WriteLine("Please enter the operator +, -, / or *: ");
-                char math_sign = char.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter the number to " + $"{math_sign}");
-
-                List<int> numbers = new List<int>();
-                while (true)
-                {
-                    Console.WriteLine("Please enter the next number: ");
-                    if (int.TryParse(Console.ReadLine(), out var number))
-                    {
-                        numbers.Add(number);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                decimal result = numbers[0];
-                var remainder = numbers.Skip(1);
-                foreach (int number in remainder)
-                {
-                    if (math_sign == '+')
-                    {
-                        result = numbers.Sum();
-                    }
-                    else if (math_sign == '-')
-                    {
-                        result = numbers.Aggregate((a, b) => a - b);
-                    }
-                    else if (math_sign == '*')
-                    {
-                        result = numbers.Aggregate((a, b) => a * b);
-                    }
-                    else if (math_sign == '/')
-                    {
-                        result = numbers.Aggregate((a, b) => a / b);
-                    }
-                }
-
-                Console.WriteLine("The answer is {0}", result);
+                DateCalculator.PerformeDateCalculation();
             }
         }
     }
 
-    class DateCalculator
+    private static int AskForCalculationMode()
+    {
+        Console.WriteLine("Which calculator do you want?");
+        Console.WriteLine("For Numbers enter 1");
+        Console.WriteLine("For Dates enter 2");
+        var choice = int.Parse(Console.ReadLine());
+        return choice;
+    }
+
+    private static void PrintWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the calculator!");
+        Console.WriteLine("=========================");
+    }
+
+    private class NumberCalculator
+    {
+        public static void PerformNumberCalcultion()
+        {
+            Console.WriteLine("Please enter the operator +, -, /, *: ");
+            var math_sign = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(math_sign) || !Enum.IsDefined(typeof(Operations), (int)math_sign[0]))
+            {
+                throw new Exception("Please enter one of these operations: +, -, *, /");
+            }
+
+            Console.WriteLine("Please enter the number to " + $"{math_sign}");
+            var numbers = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Please enter the next number: ");
+                if (int.TryParse(Console.ReadLine(), out var number))
+                    numbers.Add(number);
+                else
+                    break;
+            }
+
+            decimal result = Decimal.MaxValue;
+            switch ((Operations) math_sign[0])
+            {
+                case Operations.Add:
+                    result = numbers.Sum();
+                    break;
+                case Operations.Subtract:
+                    result = numbers.Aggregate((a, b) => a - b);
+                    break;
+                case Operations.Multiply:
+                    result = numbers.Aggregate((a, b) => a * b);
+                    break;
+                case Operations.Divide:
+                    result = numbers.Aggregate((a, b) => a / b);
+                    break;
+            }
+
+            Console.WriteLine("The answer is {0}", result);
+        }
+    }
+
+    private class DateCalculator
     {
         public static void PerformeDateCalculation()
         {
             Console.WriteLine("Please enter a date in format mm/dd/yyyy: ");
-            DateTime date = DateTime.Parse(Console.ReadLine());
+            var date = DateTime.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter the number of days to add: ");
-            int daysToAdd = int.Parse(Console.ReadLine());
+            var daysToAdd = int.Parse(Console.ReadLine());
 
             Console.WriteLine(date.AddDays(daysToAdd).ToShortDateString());
         }
